4dfd547efb36f154dd29294590a891158230df74
Gusarich <danielsedovzzz@gmail.com>
2024-07-22 09:01:17 +0300

    chore: attempt to fix

--- theme.config.jsx
+++ theme.config.jsx
@@ -21,4 +21,28 @@ export default {
             titleTemplate: '%s – Housify',
         };
     },
+    head: () => (
+        <>
+            <meta
+                name="viewport"
+                content="width=device-width, initial-scale=1.0"
+            />
+            <meta name="description" content="Housify Documentation" />
+            <meta name="og:title" content="Housify Documentation" />
+            <meta name="og:description" content="Housify Documentation" />
+            <meta name="apple-mobile-web-app-title" content="Housify" />
+            <script
+                lang="javascript"
+                dangerouslySetInnerHTML={{
+                    __html: `if (!window.localStorage.getItem("theme_default")) {
+                window.localStorage.setItem("theme", "dark");
+                window.localStorage.setItem("theme_default", "dark");
+                document.documentElement.classList.add("dark");
+                document.documentElement.classList.remove("light");
+              }`,
+                }}
+            />
+            ;
+        </>
+    ),
 };
