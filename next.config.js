c7baf49d9243901611473445612b972de220c196
nostromo <jefremovmark@gmail.com>
2024-07-30 20:10:47 +0300

    feat: add syntax highlighting

--- next.config.js
+++ next.config.js
@@ -1,10 +1,36 @@
 import nextra from 'nextra';
+import fs from 'fs';
+import { getHighlighter, BUNDLED_LANGUAGES } from 'shiki';
+
+const housify = JSON.parse(
+    fs.readFileSync('./grammars/housify.tmLanguage.json'),
+    'utf8',
+);
+
+const rehypePrettyCodeOptions = {
+    getHighlighter: (options) =>
+        getHighlighter({
+            ...options,
+            langs: [
+                ...BUNDLED_LANGUAGES,
+                {
+                    id: 'Housify',
+                    scopeName: 'source.hsf',
+                    grammar: housify,
+                    aliases: ['hsf', 'housify'],
+                },
+            ],
+        }),
+};
 
 const withNextra = nextra({
     theme: 'nextra-theme-docs',
     themeConfig: './theme.config.jsx',
     latex: true,
     defaultShowCopyCode: true,
+    mdxOptions: {
+        rehypePrettyCodeOptions,
+    },
 });
 
 export default withNextra({
